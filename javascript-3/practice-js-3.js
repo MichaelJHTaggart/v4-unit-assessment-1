a2c8ac1255f255a1cd150b5e3dc094dc7449a777
Michael Taggart <mtaggarthawk@gmail.com>
2020-11-06 15:59:10 -0700

    "Javascript Problem 3 Completed"

--- javascript-3/practice-js-3.js
+++ javascript-3/practice-js-3.js
@@ -1,12 +1,11 @@
 //////////////////STEP 1////////////////////
 /* Create an array called 'groceries' that contains three grocery items of your choice as strings. */
 
-//CODE HERE
-
+let groceries = ['bananas', 'more bananas', 'even more bananas']
 //////////////////STEP 2////////////////////
 /* Create a variable called 'yourName' that is assigned the value of your name as a string. */
 
-//CODE HERE
+let yourName = "Michael Taggart"
 
 //////////////////STEP 3////////////////////
 /*
@@ -16,7 +15,9 @@
     the function would return 'Joseph's Grocery List'.
 */
 
-//CODE HERE
+function setGroceryListTitle(x){
+    return x + "'s Grocery List"
+}
 
 //////////////////STEP 4////////////////////
 /* 
@@ -26,7 +27,10 @@
     that takes care of some of the work for getting your page to display correctly.
 */
 
-//CODE HERE
+function addItem(item){
+    groceries.push(item)
+    displayData()
+}
 
 //////////////////STEP 5////////////////////
 /*
@@ -36,7 +40,10 @@
     Then, invoke the displayData() function inside of your removeItem function.
 */
 
-//CODE HERE
+function removeItem(index){
+    groceries.splice(index)
+    displayData()
+}
 
 //////////////////STEP 6////////////////////
 /*
@@ -49,7 +56,15 @@
     with NUMBEROFGROCERIES replaced by the number of items in your groceries array.
 */
 
-//CODE HERE
+function checkGroceryCount(){
+    if(groceries.length > 5){
+        return "That looks like a big trip"
+    } else if(groceries.length === 1){
+        return "1 item"
+    } else if (groceries.length >= 1) groceries.length <= 5; {
+        return groceries.length + " items"   
+    }
+}
 
 
 //////////////////Check out your code!////////////////////
